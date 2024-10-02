9a81af59371a36246e452e72ed3e96cf067896f1
danielrogel <sagyrogel1@gmail.com>
2024-10-02 17:11:38 +0300

    done!

--- code.js
+++ code.js
@@ -1,6 +1,26 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("#btn");
 let size = 16;
+
+
+
+function createGrid(size) {
+    for (let index = 0; index < size; index++) {
+        div = document.createElement("div")
+        div.setAttribute("class", "row")
+        for (let j = 0; j < size; j++) {
+            const element = document.createElement("div")
+            element.setAttribute("class", "block")
+            element.addEventListener("mouseover", (event) => {
+                let op = window.getComputedStyle(element).getPropertyValue("opacity");
+                element.style.opacity = parseFloat(op) + 0.2;
+            });
+            div.appendChild(element)
+        }
+        
+        container.appendChild(div);
+    }
+}
 btn.addEventListener("click", (event) => {
     size = parseInt(prompt("Enter a Value", "0"));
     if (size < 101) {
@@ -9,42 +29,8 @@ btn.addEventListener("click", (event) => {
         while (container.lastElementChild) {
             container.removeChild(container.lastElementChild);
         }
-        for (let index = 0; index < size; index++) {
-            div = document.createElement("div")
-            div.setAttribute("class", "row")
-            for (let j = 0; j < size; j++) {
-                const element = document.createElement("div")
-                element.setAttribute("class", "block")
-                element.addEventListener("mouseover", (event) => {
-                    element.style.backgroundColor = "blue"
-                });
-        
-                element.addEventListener("mouseout", (event) => {
-                    element.style.backgroundColor = "red"
-                });
-                div.appendChild(element)
-            }
-            
-            container.appendChild(div);
-        }
+        createGrid(size)
     }
 });
 
-
-for (let index = 0; index < size; index++) {
-    div = document.createElement("div")
-    div.setAttribute("class", "row")
-    for (let j = 0; j < size; j++) {
-        const element = document.createElement("div")
-        element.setAttribute("class", "block")
-        element.addEventListener("mouseover", (event) => {
-            element.style.backgroundColor = "blue"
-        });
-
-        element.addEventListener("mouseout", (event) => {
-            element.style.backgroundColor = "red"
-        });
-        div.appendChild(element)
-    }
-    container.appendChild(div);
-}
+createGrid(size)
